97c6cf77f7a4f9079c64977cc42071ca1f3eb53d
ming.ma <isdom.maming@gmail.com>
2018-04-12 19:50:15 +0800

    using lambda expression

--- src/main/java/org/jocean/redis/RedisUtil.java
+++ src/main/java/org/jocean/redis/RedisUtil.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package org.jocean.redis;
 
@@ -31,52 +31,52 @@ import rx.functions.Func1;
 public class RedisUtil {
     private static final Logger LOG =
             LoggerFactory.getLogger(RedisUtil.class);
-    
+
     public static final String REDIS_OK = "OK";
-    
+
     private RedisUtil() {
         throw new IllegalStateException("No instances!");
     }
-    
+
     public static Observable<RedisMessage> cmdAuth(final String passwd) {
         return Observable.<RedisMessage>just(RedisUtil.strs2array("AUTH", passwd));
     }
-    
+
     public static Observable<RedisMessage> cmdInfo() {
         return Observable.<RedisMessage>just(RedisUtil.strs2array("INFO"));
     }
-    
+
     public static Observable<RedisMessage> cmdSelect(final int dbno) {
         return Observable.<RedisMessage>just(RedisUtil.strs2array("SELECT", Integer.toString(dbno)));
     }
-    
+
     public static class SetCommand {
-        
+
         SetCommand(final String key, final String value) {
             this._key = key;
             this._value = value;
         }
-        
+
         public SetCommand px(final long milliseconds) {
             this._px = Long.toString(milliseconds);
             return this;
         }
-        
+
         public SetCommand ex(final long seconds) {
             this._ex = Long.toString(seconds);
             return this;
         }
-        
+
         public SetCommand xx() {
             this._xx = true;
             return this;
         }
-        
+
         public SetCommand nx() {
             this._nx = true;
             return this;
         }
-        
+
         public Observable<RedisMessage> build() {
             final List<String> cmds = new ArrayList<>();
             cmds.add("SET");
@@ -98,38 +98,38 @@ public class RedisUtil {
             }
             return Observable.<RedisMessage>just(RedisUtil.strs2array(cmds.toArray(new String[0])));
         }
-        
+
         private final String _key;
         private final String _value;
-        
+
         private String _ex = null;
         private String _px = null;
         private boolean _nx = false;
         private boolean _xx = false;
     }
-    
+
     public static SetCommand cmdSet(final String key, final String value) {
         return new SetCommand(key, value);
     }
-    
+
     public static Observable<RedisMessage> cmdGet(final String key) {
         return Observable.<RedisMessage>just(RedisUtil.strs2array("GET", key));
     }
-    
+
     public static Observable<RedisMessage> cmdDel(final String... keys) {
         final List<String> cmds = new ArrayList<>();
         cmds.add("DEL");
         cmds.addAll(Arrays.asList(keys));
         return Observable.<RedisMessage>just(RedisUtil.strs2array(cmds.toArray(new String[0])));
     }
-    
+
     public static Observable<RedisMessage> error(final String errorMessage) {
         return Observable.<RedisMessage>error(new RuntimeException(errorMessage));
     }
-    
+
     public static ArrayRedisMessage strs2array(final String... strs) {
         final List<RedisMessage> children = new ArrayList<>(strs.length);
-        for (String cmd : strs) {
+        for (final String cmd : strs) {
             children.add(str2bulk(cmd));
         }
         return new ArrayRedisMessage(children);
@@ -139,7 +139,7 @@ public class RedisUtil {
         return new FullBulkStringRedisMessage(
             Unpooled.wrappedBuffer(str.getBytes(CharsetUtil.UTF_8)));
     }
-    
+
     public static boolean isOK(final RedisMessage message) {
         if (message instanceof SimpleStringRedisMessage) {
             final SimpleStringRedisMessage simplemsg = (SimpleStringRedisMessage)message;
@@ -150,124 +150,86 @@ public class RedisUtil {
         }
         return false;
     }
-    
+
     public static Func1<RedisMessage, Observable<RedisMessage>> ifOKThenElse(
-            final Observable<RedisMessage> thenRequest, 
+            final Observable<RedisMessage> thenRequest,
             final Observable<RedisMessage> elseRequest) {
-        return new Func1<RedisMessage, Observable<RedisMessage>>() {
-                @Override
-                public Observable<RedisMessage> call(final RedisMessage resp) {
-                    if (RedisUtil.isOK(resp)) {
-                        return thenRequest;
-                    } else {
-                        return elseRequest;
-                    }
-                }};
+        return resp -> RedisUtil.isOK(resp) ? thenRequest : elseRequest;
     }
-    
+
     @SafeVarargs
     public static Transformer<? super RedisConnection, ? extends RedisConnection>
         composite(final Transformer<? super RedisConnection, ? extends RedisConnection> ... transformers) {
-        return new Transformer<RedisConnection, RedisConnection>() {
-            @Override
-            public Observable<RedisConnection> call(final Observable<RedisConnection> source) {
+        return source -> {
                 Observable<RedisConnection> transformered = source;
-                for (Transformer<? super RedisConnection, ? extends RedisConnection> t : transformers) {
+                for (final Transformer<? super RedisConnection, ? extends RedisConnection> t : transformers) {
                     transformered = transformered.compose(t);
                 }
                 return transformered;
-            }
-        };
+            };
     }
-    
-    public static Transformer<? super RedisConnection, ? extends RedisConnection> 
-        authRedis(final String passwd) {
-        return new Transformer<RedisConnection, RedisConnection>() {
-            @Override
-            public Observable<RedisConnection> call(final Observable<RedisConnection> source) {
+
+    @SuppressWarnings("finally")
+    public static Transformer<? super RedisConnection, ? extends RedisConnection> authRedis(final String passwd) {
+        return source->{
                 if (null == passwd || (null != passwd && passwd.isEmpty())) {
                     return source;
                 } else {
-                    return source.flatMap(new Func1<RedisConnection, Observable<RedisConnection>>() {
-                    @Override
-                    public Observable<RedisConnection> call(final RedisConnection conn) {
-                        return conn.defineInteraction(cmdAuth(passwd))
-                        .flatMap(new Func1<RedisMessage, Observable<RedisConnection>>() {
-                            @Override
-                            public Observable<RedisConnection> call(final RedisMessage resp) {
+                    return source.flatMap(conn->conn.defineInteraction(cmdAuth(passwd))
+                        .map(resp->{
                                 if (LOG.isDebugEnabled()) {
                                     LOG.debug("receive redis resp[{}] for auth", resp);
                                 }
                                 if (isOK(resp)) {
-                                    return Observable.<RedisConnection>just(conn);
+                                    return conn;
                                 } else {
-                                    // close redis connection first
-                                    conn.close();
-                                    return Observable.<RedisConnection>error(
-                                            new RuntimeException("Auth Failed"));
+                                    try {
+                                        // close redis connection first
+                                        conn.close();
+                                    } finally {
+                                        throw new RuntimeException("Auth Failed");
+                                    }
                                 }
-                            }});
-                    }});
+                            })
+                    );
                 }
-            }
         };
     }
-    
-    public static Transformer<? super RedisConnection, ? extends RedisConnection> 
-        selectDB(final int dbno) {
-        return new Transformer<RedisConnection, RedisConnection>() {
-            @Override
-            public Observable<RedisConnection> call(final Observable<RedisConnection> source) {
-                return source.flatMap(new Func1<RedisConnection, Observable<RedisConnection>>() {
-                    @Override
-                    public Observable<RedisConnection> call(final RedisConnection conn) {
-                        return conn.defineInteraction(cmdSelect(dbno))
-                        .flatMap(new Func1<RedisMessage, Observable<RedisConnection>>() {
-                            @Override
-                            public Observable<RedisConnection> call(final RedisMessage resp) {
+
+    @SuppressWarnings("finally")
+    public static Transformer<? super RedisConnection, ? extends RedisConnection> selectDB(final int dbno) {
+        return source->source.flatMap(conn->conn.defineInteraction(cmdSelect(dbno))
+                        .map(resp->{
                                 if (LOG.isDebugEnabled()) {
                                     LOG.debug("receive redis resp[{}] for select", resp);
                                 }
                                 if (isOK(resp)) {
-                                    return Observable.<RedisConnection>just(conn);
+                                    return conn;
                                 } else {
-                                    // close redis connection first
-                                    conn.close();
-                                    return Observable.<RedisConnection>error(
-                                            new RuntimeException("Select Failed"));
+                                    try {
+                                        // close redis connection first
+                                        conn.close();
+                                    } finally {
+                                        throw new RuntimeException("Select Failed");
+                                    }
                                 }
-                            }});
-                    }});
-            }
-        };
+                            })
+                    );
     }
-    
-    public static Transformer<? super RedisConnection, ? extends RedisMessage> 
-        interactWithRedis(final Observable<RedisMessage> first, 
-                @SuppressWarnings("unchecked") 
+
+    public static Transformer<? super RedisConnection, ? extends RedisMessage>
+        interactWithRedis(final Observable<RedisMessage> first,
+                @SuppressWarnings("unchecked")
                 final Func1<RedisMessage, Observable<RedisMessage>>... extras) {
-        return new Transformer<RedisConnection, RedisMessage>() {
-            @Override
-            public Observable<RedisMessage> call(final Observable<RedisConnection> source) {
-                return source.flatMap(new Func1<RedisConnection, Observable<? extends RedisMessage>>() {
-                    @Override
-                    public Observable<? extends RedisMessage> call(final RedisConnection conn) {
+        return source->source.flatMap(conn -> {
                         Observable<? extends RedisMessage> resp = conn.defineInteraction(first);
-                        for (Func1<RedisMessage, Observable<RedisMessage>> next : extras ) {
-                            final Func1<RedisMessage, Observable<RedisMessage>> doNext = next;
-                            resp = resp.flatMap(new Func1<RedisMessage, Observable<? extends RedisMessage>>() {
-                                @Override
-                                public Observable<? extends RedisMessage> call(final RedisMessage msg) {
-                                    return conn.defineInteraction(doNext.call(msg));
-                                }});
+                        for (final Func1<RedisMessage, Observable<RedisMessage>> next : extras ) {
+                            resp = resp.flatMap(msg->conn.defineInteraction(next.call(msg)));
                         }
                         return resp.doAfterTerminate(conn.closer());
-                    }})
-                    ;
-            }
-        };
+                    });
     }
-    
+
     public static String dumpAggregatedRedisMessage(final RedisMessage msg) {
         final StringBuilder sb = new StringBuilder();
         if (msg instanceof SimpleStringRedisMessage) {
@@ -279,17 +241,17 @@ public class RedisUtil {
         } else if (msg instanceof FullBulkStringRedisMessage) {
             sb.append(getString((FullBulkStringRedisMessage) msg));
         } else if (msg instanceof ArrayRedisMessage) {
-            for (RedisMessage child : ((ArrayRedisMessage) msg).children()) {
+            for (final RedisMessage child : ((ArrayRedisMessage) msg).children()) {
                 sb.append(child);
                 sb.append('\n');
             }
         } else {
             throw new CodecException("unknown message type: " + msg);
         }
-        
+
         return sb.toString();
     }
-    
+
     private static String getString(final FullBulkStringRedisMessage msg) {
         if (msg.isNull()) {
             return "(null)";
